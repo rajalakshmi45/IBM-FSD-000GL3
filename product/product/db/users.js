65705aa16d1d864c8780142355d4f8f3e539671a
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2019-07-23 22:27:44 +0530

    Update users.js

--- product/product/db/users.js
+++ product/product/db/users.js
@@ -1,20 +1,20 @@
 const product = [
     {
         product_id : 1,
-        product_name : "Pen",
-        product_price : 25,
+        product_name : "washing machine",
+        product_price : 25000,
         product_quantity : 10
     },
     {
         product_id : 2,
-        product_name : "Eraser",
-        product_price : 10,
+        product_name : "fridge",
+        product_price : 10000,
         product_quantity : 20
     },
     {
         product_id : 3,
-        product_name : "Scale",
-        product_price : 15,
+        product_name : "microwave oven",
+        product_price : 1500,
         product_quantity : 15
     }
 ]
